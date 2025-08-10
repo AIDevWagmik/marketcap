7b5de7288a10f59873a0822707eb0e482498a752
AIDevWagmik <buschertom92@gmail.com>
2025-08-11 00:54:19 +0200

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,23 +1,31 @@
 const CACHE = "mcap-buy-v1";
 const ASSETS = [
-  "/app.html",
-  "/buy.png",
-  "buy-192.png",
-  "buy-512.png",
-  "/manifest.webmanifest"
+  "./",               // covers index.html when served as a directory index
+  "./index.html",
+  "./manifest.json",
+  "./buy.png",
+  "./icons/icon-192.png",
+  "./icons/icon-512.png"
 ];
 
-self.addEventListener("install", e=>{
-  e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)));
+self.addEventListener("install", e => {
+  e.waitUntil(caches.open(CACHE).then(c => c.addAll(ASSETS)));
   self.skipWaiting();
 });
-self.addEventListener("activate", e=>{
-  e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))));
+
+self.addEventListener("activate", e => {
+  e.waitUntil(
+    caches.keys().then(keys =>
+      Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k)))
+    )
+  );
   self.clients.claim();
 });
-self.addEventListener("fetch", e=>{
+
+self.addEventListener("fetch", e => {
   const url = new URL(e.request.url);
-  if (ASSETS.includes(url.pathname)) {
-    e.respondWith(caches.match(e.request));
+  // Cache-first for app shell and listed assets
+  if (e.request.mode === "navigate" || ASSETS.includes("." + url.pathname.split(location.pathname.replace(/[^/]+$/, "")).pop())) {
+    e.respondWith(caches.match(e.request).then(cached => cached || fetch(e.request)));
   }
 });
